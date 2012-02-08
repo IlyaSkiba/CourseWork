d951fe7aa37681deaef154b86da71e40e38b8c99
unknown <Meloman@meloman-note.(none)>
2012-02-09 00:21:39 +0300

    ChangePassword started

--- src/main/java/org/jgeek/website/model/global/UserModel.java
+++ src/main/java/org/jgeek/website/model/global/UserModel.java
@@ -3,7 +3,10 @@ package org.jgeek.website.model.global;
 import org.jgeek.website.model.security.UserAccount;
 import org.jgeek.website.service.JpaUserDetailsService;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -20,11 +23,15 @@ public class UserModel {
 
     @Inject
     JpaUserDetailsService userService;
+    @Inject
+    ShaPasswordEncoder passwordEncoder;
 
     private String userName;
     private String firstName;
     private String middleName;
     private String lastName;
+    private String oldPassword;
+    private String newPassword;
 
     public void initializeByUsername() {
         UserAccount userDto = (UserAccount) userService.loadUserByUsername(userName);
@@ -64,4 +71,31 @@ public class UserModel {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public void changePassword() {
+        if (passwordEncoder.isPasswordValid(userService.loadUserByUsername(userName).getPassword(), oldPassword, null)) {
+
+            userService.changePassword(userName, newPassword);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Пароль успешно сменен", null));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Введенный пароль не верен", null));
+        }
+    }
+
 }
