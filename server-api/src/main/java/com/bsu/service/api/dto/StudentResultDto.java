9960001bd4a8b2410145552232b4d7ba3f2dc163
KiraStepina <fpmi.student@mail.ru>
2013-05-28 23:15:48 +0300

    Theoretic Test passing fix

--- server-api/src/main/java/com/bsu/service/api/dto/StudentResultDto.java
+++ server-api/src/main/java/com/bsu/service/api/dto/StudentResultDto.java
@@ -8,4 +8,31 @@ package com.bsu.service.api.dto;
  * To change this template use File | Settings | File Templates.
  */
 public class StudentResultDto {
+    private String userName;
+    private String testName;
+    private Integer resultScore;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public Integer getResultScore() {
+        return resultScore;
+    }
+
+    public void setResultScore(Integer resultScore) {
+        this.resultScore = resultScore;
+    }
 }
